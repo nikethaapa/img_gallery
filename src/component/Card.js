3dc51bf69187b7afb029ca7dfd98fe1e74214b32
puvinpks <puvin.s@datamoo.ai>
2026-04-05 16:07:52 +0530

    con

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,16 +1,15 @@
+// src/components/Card.js
+import React from "react";
 
-
-function Card({title,imglink}){
-    return (
-        <div className='img_card'>
-
-       
-        <div className="card">
-            <img src={imglink} alt={title} />
-        </div>
-        <h2 className="img_title">{title}</h2>
-         </div>
-    )
+function Card({ title, img }) {
+  return (
+    <div className='img_card'>
+      <div className="card">
+        <img src={img} alt={title} />
+      </div>
+      <h2 className="img_title">{title}</h2>
+    </div>
+  );
 }
 
 export default Card;
--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -1,62 +1,59 @@
+// src/components/Gallery.js
+
 import Card from "./Card";
 
-import i1 from "./asset/1.jpg";
-import i2 from "./asset/2.jpg";
-import i3 from "./asset/3.jpg";
-import i4 from "./asset/4.jpg"; 
-import i5 from "./asset/5.jpg"; 
-import i6 from "./asset/6.jpg"; 
-import i7 from "./asset/7.jpg"; 
-import i8 from "./asset/8.jpg"; 
-import i9 from "./asset/9.jpg"; 
-import i10 from "./asset/10.jpg"; 
-import i11 from "./asset/11.jpg"; 
-import i12 from "./asset/12.jpg"; 
-import i13 from "./asset/13.jpg"; 
-import i14 from "./asset/14.jpg"; 
-import i15 from "./asset/15.jpg"; 
-import i16 from "./asset/16.jpg"; 
-import i17 from "./asset/17.jpg"; 
-import i18 from "./asset/18.jpg"; 
-import i19 from "./asset/19.jpg"; 
-import i20 from "./asset/20.jpg";
+import img1 from "./asset/1.jpg";
+import img2 from "./asset/2.jpg";
+import img3 from "./asset/3.jpg";
+import img4 from "./asset/4.jpg";
+import img5 from "./asset/5.jpg";
+import img6 from "./asset/6.jpg";
+import img7 from "./asset/7.jpg";
+import img8 from "./asset/8.jpg";
+import img9 from "./asset/9.jpg";
+import img10 from "./asset/10.jpg";
+import img11 from "./asset/11.jpg";
+import img12 from "./asset/12.jpg";
+import img13 from "./asset/13.jpg";
+import img14 from "./asset/14.jpg";
+import img15 from "./asset/15.jpg";
+import img16 from "./asset/16.jpg";
+import img17 from "./asset/17.jpg";
+import img18 from "./asset/18.jpg";
+import img19 from "./asset/19.jpg";
+import img20 from "./asset/20.jpg";
 
+// Array of images
 const galleryData = [
-  { id: 1, title: "Sleepy Whiskers", image: i1 },
-  { id: 2, title: "Curious Kitty", image: i2 },
-  { id: 3, title: "Tiny Paws", image: i3 },
-  { id: 4, title: "Fluffy Cloud", image: i4 },
-  { id: 5, title: "Golden Eyes", image: i5 },
-  { id: 6, title: "Lazy Afternoon", image: i6 },
-  { id: 7, title: "Playful Mood", image: i7 },
-  { id: 8, title: "Sneaky Hunter", image: i8 },
-  { id: 9, title: "Nap Time", image: i9 },
-  { id: 10, title: "Cute Stare", image: i10 },
-  { id: 11, title: "Jumping Joy", image: i11 },
-  { id: 12, title: "Soft Fur", image: i12 },
-  { id: 13, title: "Window Watcher", image: i13 },
-  { id: 14, title: "Silent Walker", image: i14 },
-  { id: 15, title: "Little Explorer", image: i15 },
-  { id: 16, title: "Cozy Corner", image: i16 },
-  { id: 17, title: "Morning Stretch", image: i17 },
-  { id: 18, title: "Happy Tail", image: i18 },
-  { id: 19, title: "Dreamy Eyes", image: i19 },
-  { id: 20, title: "Purr Machine", image: i20 }
+  { title: "Cat 1", img: img1 },
+  { title: "Cat 2", img: img2 },
+  { title: "Cat 3", img: img3 },
+  { title: "Cat 4", img: img4 },
+  { title: "Cat 5", img: img5 },
+  { title: "Cat 6", img: img6 },
+  { title: "Cat 7", img: img7 },
+  { title: "Cat 8", img: img8 },
+  { title: "Cat 9", img: img9 },
+  { title: "Cat 10", img: img10 },
+  { title: "Cat 11", img: img11 },
+  { title: "Cat 12", img: img12 },
+  { title: "Cat 13", img: img13 },
+  { title: "Cat 14", img: img14 },
+  { title: "Cat 15", img: img15 },
+  { title: "Cat 16", img: img16 },
+  { title: "Cat 17", img: img17 },
+  { title: "Cat 18", img: img18 },
+  { title: "Cat 19", img: img19 },
+  { title: "Cat 20", img: img20 },
 ];
 
 function Gallery() {
   return (
-    <section id="gallery" className="gallery-section">
-  <h2 className="section-title" style={{ textAlign: "center" }}>
-    Gallery
-  </h2>
-
-  <div className="gallery">
-    {galleryData.map((img) => (
-      <Card key={img.id} title={img.title} imglink={img.image} />
-    ))}
-  </div>
-</section>
+    <div className="gallery">
+      {galleryData.map((item, index) => (
+        <Card key={index} title={item.title} img={item.img} />
+      ))}
+    </div>
   );
 }
 
